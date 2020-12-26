db9a9dc3679ddd5e7fea8304fcf0bc21da09293c
Wasabi Burns <69662763+spiceywasabi@users.noreply.github.com>
2020-12-25 22:18:09 -0800

    add stty since its needed

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ MIPSGCCPATH="" # path to MIPS compiler
 #	*mips*)
 
 selected_packages+="-ppp -ppp-mod-pppoe -kmod-ppp -kmod-pppoe -kmod-pppox gpioctl-sysfs kmod-leds-gpio kmod-usb-acm -firewall -ip6tables -kmod-nf-conntrack6 -kmod-ip6tables -kmod-ipv6 -odhcp6c "
-selected_packages+="screen python-light python-logging python-multiprocessing python-pyserial nano lsof logd kmod-gpio-button-hotplug e2fsprogs htop kmod-sdhci kmod-mt76-core kmod-mt76-usb htop serialconsole setserial "
+selected_packages+="screen python-light python-logging python-multiprocessing python-pyserial nano lsof logd kmod-gpio-button-hotplug e2fsprogs htop kmod-sdhci kmod-mt76-core kmod-mt76-usb htop serialconsole setserial coreutils-stty "
 selected_packages+="kmod-fs-ext4 block-mount kmod-usb-storage kmod-usb-storage kmod-usb-storage-extras kmod-rt2800-usb kmod-sdhci-mt7620 block-mount kmod-fs-vfat kmod-nls-cp437 kmod-nls-utf8 kmod-nls-iso8859-1 "
 selected_packages+="luci luci-compat luci-ssl python-pip "
 #xinetd muninlite
