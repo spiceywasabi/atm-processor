4817614730946a48231714518266726b3b79eb1d
wasabi <wasabi@dc562.org>
2020-12-27 12:17:26 -0800

    fix wrong path

--- files/atm/atm-runner.sh
+++ files/atm/atm-runner.sh
@@ -2,7 +2,7 @@
 # basically just another hand off but at least we don't need new firmware each time we need to make a change
 # 1. call set seriall
 # 2. launch 
-cd /mnt/atm/processor
+cd /atm/
 killall -9 screen
 killall -9 screen
 screen -wipe
